114e82effa8d843fca48229bd32bb196641d04fd
Lachlan Ross <lachlan.f.ross@gmail.com>
2023-05-15 00:17:28 +0800

    Backtesting must also enter entryzone first

--- backtesting.py
+++ backtesting.py
@@ -4,9 +4,11 @@ import config
 
 client = config.get_binance_config()
 
-def get_sample_data():
-    return ['MAGICUSDT', client.KLINE_INTERVAL_1MINUTE, '1684059363825', 0.836, [0.84043,0.85021,0.85941,0.90288], 0.7661]
+def get_sample_long_data():
+    return ['MAGICUSDT', client.KLINE_INTERVAL_5MINUTE, '1684059363825', 0.837, [0.84043,0.85021,0.85941,0.90288], 0.7661]
 
+def get_sample_short_data():
+    return ['ETHUSDT', client.KLINE_INTERVAL_5MINUTE, '1683970193138', 1830, [1820.85, 1811.7, 1793.4, 1740.33, 1692.0], 1955.2]
 
 def get_historical_klines(symbol, interval, start_str):
     end_str = int(start_str) + (1000 * 60 * 60 * 24 * 7)
@@ -15,36 +17,58 @@ def get_historical_klines(symbol, interval, start_str):
     print(data)
     return data
 
-def calculate_profit_percentage(entry_price, target_price):
-    return round(((target_price - entry_price) / entry_price) * 100, 2)
+def calculate_profit_percentage(entry_price, target_price, direction):
+    if direction == 'LONG':
+        return round(((target_price - entry_price) / entry_price) * 100, 2)
+    elif direction == 'SHORT':
+        return round(((entry_price - target_price) / entry_price) * 100, 2)
 
-def backtest(data, entry_price, take_profit_values, stop_loss, start_time):
+
+def backtest(data, entry_price, take_profit_values, stop_loss, start_time, direction):
+    start_time = int(start_time)
+    entered = False
+
+
     data['high'] = pd.to_numeric(data['high'])
     data['low'] = pd.to_numeric(data['low'])
 
-    take_profit_values.sort()
+    if direction == 'LONG':
+        take_profit_values.sort()
+    elif direction == 'SHORT':
+        take_profit_values.sort(reverse=True)
+
     take_profit_hits = 0
     take_profit_times = {}
     take_profit_percentages = {}
 
     for index, row in data.iterrows():
-        if row['low'] <= stop_loss:
-            stop_loss_percentage = calculate_profit_percentage(entry_price, stop_loss)
+        if direction == 'LONG' and row['low'] <= entry_price:
+            entered = True
+        elif direction == 'SHORT' and row['high'] >= entry_price:
+            entered = True
+        if not entered:
+            continue
+        if (direction == 'LONG' and row['low'] <= stop_loss) or (direction == 'SHORT' and row['high'] >= stop_loss):
+            stop_loss_percentage = calculate_profit_percentage(entry_price, stop_loss, direction)
             return ('stop_loss', stop_loss, row['open_time'], take_profit_hits, take_profit_times, stop_loss_percentage, take_profit_percentages)
         for take_profit in take_profit_values:
-            if row['high'] >= take_profit:
+            if (direction == 'LONG' and row['high'] >= take_profit) or (direction == 'SHORT' and row['low'] <= take_profit):
                 take_profit_hits += 1
-                take_profit_times[take_profit] = round((row['open_time'] - int(start_time)) / (1000 * 60 * 60), 2)  # Time difference in hours
-                take_profit_percentages[take_profit] = calculate_profit_percentage(entry_price, take_profit)
+                take_profit_times[take_profit] = round((row['open_time'] - start_time) / (1000 * 60 * 60), 2)  # Time difference in hours
+                take_profit_percentages[take_profit] = calculate_profit_percentage(entry_price, take_profit, direction)
                 take_profit_values.remove(take_profit)
+    if not entered:
+        return ('not_entered', None, None, take_profit_hits, take_profit_times, None, take_profit_percentages)
     return ('time_out', None, None, take_profit_hits, take_profit_times, None, take_profit_percentages)
 
 
 
+
+
 def run_backtest():
-    backtesting_data = get_sample_data()
+    backtesting_data = get_sample_long_data()
     kline_data = get_historical_klines(backtesting_data[0],backtesting_data[1],backtesting_data[2])
-    result_type, result_price, result_time, take_profit_hits, take_profit_times, stop_loss_percentage, take_profit_percentages = backtest(kline_data, backtesting_data[3], backtesting_data[4], backtesting_data[5], backtesting_data[2])
+    result_type, result_price, result_time, take_profit_hits, take_profit_times, stop_loss_percentage, take_profit_percentages = backtest(kline_data, backtesting_data[3], backtesting_data[4], backtesting_data[5], backtesting_data[2], 'LONG')
 
     print(f"Result: {result_type} at price {result_price} and time {result_time}")
     print(f"Number of Take Profits hit: {take_profit_hits}")
